e1acde1fc24b83353b062b1265cb4f3bcfc3dea9
Guga <contato.gustavobarbosa@outlook.com>
2022-05-17 15:15:14 -0300

    Preparando revisão de código

--- Routers/movRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getMov, postMov} from "../Controllers/movController.js"
-
-const movRouter = Router();
-
-movRouter.post("/movimentacoes", postMov);
-movRouter.get("/movimentacoes", getMov);
-
-export default movRouter;
--- Routers/usersRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { cadastrar, login} from "../Controllers/usersController.js"
-
-const usersRouter = Router();
-
-usersRouter.post("/cadastrar", cadastrar);
-usersRouter.post("/login", login);
-
-export default usersRouter;
